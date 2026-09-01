c99939bac94562567cbcc0cb0702e2868718fdd6
barakaramadhan <barakaramadhan444@gmail.com>
2026-09-01 13:28:49 +0700

    Resolve navbar merge conflict & memunculkan dropdown

--- Navigasi/navbar.js
+++ Navigasi/navbar.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
+  // Klik tombol Fitur
+  $("#btn-fitur").on("click", function (e) {
+    e.stopPropagation();
 
-    $(document).on('click', '#btn-fitur', function () {
-        $(this).find('svg').toggleClass('rotate-180');
-    });
+    // Muter icon SVG
+    $(this).find("svg").toggleClass("rotate-180");
 
+    // Buka / tutup dropdown
+    $(this)
+      .find("ul")
+      .toggleClass("opacity-0 invisible translate-y-2")
+      .toggleClass("opacity-100 visible translate-y-0");
+  });
+
+  // Klik di luar dropdown
+  $(document).on("click", function (e) {
+    if (!$(e.target).closest("#btn-fitur").length) {
+      // Kembalikan icon
+      $("#btn-fitur svg").removeClass("rotate-180");
+
+      // Tutup dropdown
+      $("#btn-fitur ul")
+        .addClass("opacity-0 invisible translate-y-2")
+        .removeClass("opacity-100 visible translate-y-0");
+    }
+  });
 });
