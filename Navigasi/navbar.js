aee3bd2bcab28e0a6e2e6a0db9874c3e72916822
barakaramadhan <barakaramadhan444@gmail.com>
2026-09-01 13:32:33 +0700

    perubahan pada navbar.js

--- Navigasi/navbar.js
+++ Navigasi/navbar.js
@@ -1,28 +1,25 @@
 $(document).ready(function () {
-  // Klik tombol Fitur
-  $("#btn-fitur").on("click", function (e) {
+
+  $(document).on('click', '#btn-fitur', function (e) {
     e.stopPropagation();
 
-    // Muter icon SVG
-    $(this).find("svg").toggleClass("rotate-180");
+    // HANYA muter SVG panah (SVG yang ada di dalam div), bukan SVG di dalam menu ul
+    $(this).find('div > svg').toggleClass('rotate-180');
 
-    // Buka / tutup dropdown
+    // Toggle dropdown menu
     $(this)
-      .find("ul")
-      .toggleClass("opacity-0 invisible translate-y-2")
-      .toggleClass("opacity-100 visible translate-y-0");
+      .find('ul')
+      .toggleClass('opacity-0 invisible translate-y-2 opacity-100 visible translate-y-0');
   });
 
-  // Klik di luar dropdown
-  $(document).on("click", function (e) {
-    if (!$(e.target).closest("#btn-fitur").length) {
-      // Kembalikan icon
-      $("#btn-fitur svg").removeClass("rotate-180");
-
-      // Tutup dropdown
-      $("#btn-fitur ul")
-        .addClass("opacity-0 invisible translate-y-2")
-        .removeClass("opacity-100 visible translate-y-0");
+  // Tutup dropdown saat klik luar
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('#btn-fitur').length) {
+      $('#btn-fitur div > svg').removeClass('rotate-180');
+      $('#btn-fitur ul')
+        .addClass('opacity-0 invisible translate-y-2')
+        .removeClass('opacity-100 visible translate-y-0');
     }
   });
+
 });
