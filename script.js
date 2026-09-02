c10eb4dd580c4eb5c44cc61714031779ffc0e21c
Nama Kamu <garuda281209@student.abudzar.sch.id>
2026-09-02 14:25:28 +0700

    update agar dropdown bisa digunakan di index.html

--- script.js
+++ script.js
@@ -4,5 +4,26 @@ $(document).ready(function () {
         
         $(this).find('img').attr('src', '../Navigasi/Logo (9).png');
     });
+
+    $(document).on('click', '#btn-fitur', function (e) {
+    e.stopPropagation();
+
+    // HANYA muter SVG panah (SVG yang ada di dalam div), bukan SVG di dalam menu ul
+    $(this).find('div > svg').toggleClass('rotate-180');
+
+    // Toggle dropdown menu
+    $(this)
+      .find('ul')
+      .toggleClass('opacity-0 invisible translate-y-2 opacity-100 visible translate-y-0');
+  });
+
+  // Tutup dropdown saat klik luar
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('#btn-fitur').length) {
+      $('#btn-fitur div > svg').removeClass('rotate-180');
+      $('#btn-fitur ul')
+        .addClass('opacity-0 invisible translate-y-2')
+        .removeClass('opacity-100 visible translate-y-0');
+    }
     
 })
