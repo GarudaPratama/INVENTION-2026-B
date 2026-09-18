d5498b099a85c442a7ac720fbc625a5e09ce36ac
Nama Kamu <garuda281209@student.abudzar.sch.id>
2026-09-18 09:41:47 +0700

    perbaikan lagi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
   // 1. Load Navbar
-  $("#navbar").load("./navigasi/navbar.html", function () {
-    $(this).find("img").attr("src", "./navigasi/Logo-9.png");
+  $("#navbar").load("./Navigasi/navbar.html", function () {
+    $(this).find("img").attr("src", "./Navigasi/Logo-9.png");
   });
 
   // Variabel penanda agar kode tidak dieksekusi terus-menerus saat scroll
